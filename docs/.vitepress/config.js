b3ff7421bff921f8ca1ac4f8f94fdc83c709e479
Ctibor Laky <laky@efabrica.sk>
2022-11-28 14:24:54 +0100

    example navigation

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,26 @@
 export default {
-    title: 'Vue3 enterprice solution docs',
-    description: 'Just playing around.'
-  }
-  
+  title: 'Vue3 enterprice solution docs',
+  description: 'Just playing around.',
+  themeConfig: {
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      }
+    ]
+  },
+};
